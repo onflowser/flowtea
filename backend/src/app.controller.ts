a7327f0a4f5a1c5cde98d7e338ce6d6c2c27bf7f
Bart <bartolomej.kozorog@gmail.com>
2022-06-02 00:32:25 +0200

    query user donations

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventEntity } from './event.entity';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Hello World';
+  constructor(
+    @InjectRepository(EventEntity)
+    private flowEventRepository: Repository<EventEntity>,
+  ) {}
+
+  @Get(':address')
+  getEventsForAddress(@Param('address') address) {
+    return this.flowEventRepository.find({
+      where: {
+        to: address,
+      },
+    });
   }
 }
