2ad00b119086bf1003523af99afb6ddcd24f8242
jakob <drusany.jakob@gmail.com>
2022-06-03 04:03:23 +0200

    Landing page and Layout redesign
    navbar, footer, first two parts

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,41 +1,94 @@
 import { ReactElement } from "react";
 import styled from "styled-components";
+import Image from "next/image";
+import Link from "next/link";
+
+// resources
+import bmft_logo from "../public/images/logo-BMFT-horizontal.svg";
+import bmft_logo_ver from "../public/images/logo-BMFT-vertical.svg";
+
+// components
+import RoundButton from "./RoundButton";
 
 type Props = {
   children: ReactElement;
-}
+};
 
-export default function Layout({ children} : Props) {
+const LinkText = styled.a`
+  text-decoration: none;
+  color: ${(props) => props.theme.colors.mainDark};
+  padding: 0 1em;
 
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 25px;
+`;
+
+export default function Layout({ children }: Props) {
   return (
     <>
       <Navigation>
-        <NavCol>How does it work ?</NavCol>
-        <NavCol>Buy Me a Flow Tea</NavCol>
-        <NavCol>Log In</NavCol>
+        <NavInner>
+          <Image src={bmft_logo} />
+          <NavigationRightButtons>
+            <Link href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} passHref>
+              <LinkText style={{ paddingRight: `2em` }}>
+                HOW DOES IT WORK?
+              </LinkText>
+            </Link>
+            <Link href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} passHref>
+              <LinkText>Log in</LinkText>
+            </Link>
+            <RoundButton href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}>
+              Sign up
+            </RoundButton>
+          </NavigationRightButtons>
+        </NavInner>
       </Navigation>
-      <Main>
-        {children}
-      </Main>
+      <Main>{children}</Main>
       <Footer>
-        <span>Buy Me a Flow Tea</span>
-        <span>Created and Supported by Flowser team.</span>
+        <FooterImageWrapper>
+          <Image src={bmft_logo_ver} />
+        </FooterImageWrapper>
+        <FooterText>
+          Buy me a FLOW tea is created and supported by{" "}
+          <Link href={"https://github.com/onflowser"}>
+            <a target="_blank">Flowser team</a>
+          </Link>
+        </FooterText>
       </Footer>
     </>
-  )
+  );
 }
 
 const Navigation = styled.nav`
-  display: flex;
-  flex-direction: row;
-  height: 100px;
-`;
+  position: absolute;
+  width: 100vw;
+  top: 0;
 
-const NavCol = styled.div`
-  flex: 1;
   display: flex;
   justify-content: center;
+`;
+
+const NavInner = styled.div`
+  display: flex;
+  flex-direction: row;
   align-items: center;
+  box-sizing: border-box;
+  justify-content: space-between;
+  width: 100%;
+  max-width: ${({ theme }) => theme.layout.max_width};
+  padding: ${({ theme }) => theme.layout.mobile_padding};
+  height: ${({ theme }) => theme.layout.navbar_height};
+
+  z-index: 2;
+`;
+
+const NavigationRightButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: end;
 `;
 
 const Main = styled.main``;
@@ -46,5 +99,18 @@ const Footer = styled.footer`
   align-items: center;
   background: ${({ theme }) => theme.colors.secondary};
   padding: ${({ theme }) => theme.gutter.xl};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 8em 0;
+`;
+
+const FooterImageWrapper = styled.div`
+  margin-bottom: 2em;
+`;
+
+const FooterText = styled.a`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 21px;
+
   color: ${({ theme }) => theme.colors.white};
 `;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import React from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Layout from "../components/Layout";
 
@@ -7,15 +8,25 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    background:#141C2D;
+    color:#141C2D;
+  }
+
+  a {
+    color:#fe6f6f;
+    text-decoration: none;
   }
 `;
 
 const colors = {
   pink: "#db537d",
   orange: "#fe6f6f",
-  white: "#fcfcfc",
+  white: "#fff",
   grey: "#f3f3f3",
   darkBlue: "#131c2d",
+  mainDark: "#141C2D",
+  lightViolet: "#E8E5FC",
 };
 
 const theme = {
@@ -27,9 +38,15 @@ const theme = {
     xl: "25px",
   },
   colors: {
-    primary: colors.pink,
+    primary: colors.orange,
     secondary: colors.darkBlue,
     ...colors,
+  },
+  // maybe only for testing
+  layout: {
+    navbar_height: "200px",
+    max_width: "1200px",
+    mobile_padding: "2rem",
   },
 };
 
