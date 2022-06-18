d0a761987c9f355735ca5e85961f56b53162ab03
Bart <bartolomej.kozorog@gmail.com>
2022-06-18 19:17:21 +0200

    fix: markdown-preview lib import

--- frontend/components/MarkdownPreview.tsx
+++ frontend/components/MarkdownPreview.tsx
@@ -1,18 +1,25 @@
 import "@uiw/react-markdown-preview/markdown.css";
-import MDEditor from "@uiw/react-md-editor";
 import { MarkdownPreviewProps } from "@uiw/react-markdown-preview";
 import styled from "styled-components";
+import dynamic from "next/dynamic";
+
+const Markdown = dynamic(() => import("@uiw/react-markdown-preview"), {
+  ssr: false,
+});
 
 type Props = MarkdownPreviewProps;
 
 export function MarkdownPreview({ ...props }: Props) {
-  // @ts-ignore
-  return <Viewer {...props} />;
+  return (
+    <Container>
+      <Markdown {...props} />
+    </Container>
+  );
 }
 
-const Viewer = styled(MDEditor.Markdown)`
-  background-color: transparent;
+const Container = styled.div`
   * {
+    background-color: transparent;
     font-family: "Poppins", sans-serif;
   }
 `;
