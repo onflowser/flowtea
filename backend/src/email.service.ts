29d34dc071e45426f2b125df8af97c0feba5a2a5
Bart <bartolomej.kozorog@gmail.com>
2022-06-25 20:12:24 +0200

    add other email templates

--- backend/src/email.service.ts
+++ backend/src/email.service.ts
@@ -3,14 +3,21 @@ import * as sendGrid from '@sendgrid/mail';
 
 export enum EmailTemplate {
   WELCOME = 'welcome',
+  TX_SENT = 'transaction-sent',
+  TX_RECEIVED = 'transaction-received',
+  RECURRING_TX_REMINDER = 'recurring-transaction-reminder',
+  RECURRING_TX_SECOND_REMINDER = 'recurring-transaction-second-reminder',
+  RECURRING_TX_EXPIRATION_REMINDER = 'recurring-transaction-expiration-reminder',
+  RECURRING_TX_CANCELED = 'recurring-transaction-canceled',
 }
 
 const emailTemplateToId = {
-  [EmailTemplate.WELCOME]: 'd-0c089f8dbf98476f894f9e358df6fa07 ',
-};
-
-const emailTemplateToDefaultSubject = {
-  [EmailTemplate.WELCOME]: 'Welcome to FlowTea!',
+  [EmailTemplate.WELCOME]: 'd-0c089f8dbf98476f894f9e358df6fa07',
+  [EmailTemplate.TX_SENT]: 'd-52fa755b69034793af08ecc8d0bf7391',
+  [EmailTemplate.TX_RECEIVED]: 'd-cdf967b5894b49dd87011dfa493eb941',
+  [EmailTemplate.RECURRING_TX_REMINDER]: 'd-20e477bb900b4895af536f5aa3e5b1e6',
+  [EmailTemplate.RECURRING_TX_SECOND_REMINDER]:
+    'd-8192d68f73af4287894df86bf8f1e8d4',
 };
 
 type SendEmailArgs<T extends EmailTemplate> = {
@@ -20,10 +27,38 @@ type SendEmailArgs<T extends EmailTemplate> = {
   templateData: EmailTemplateData[T];
 };
 
+type TransactionParams = {
+  flowAmount: number;
+  receiverName: string;
+  receiverAddress: string;
+  transactionDate: string;
+};
+
 type EmailTemplateData = {
   [EmailTemplate.WELCOME]: {
     name: string;
   };
+  [EmailTemplate.TX_SENT]: Pick<
+    TransactionParams,
+    'flowAmount' | 'receiverAddress'
+  >;
+  [EmailTemplate.TX_RECEIVED]: Pick<TransactionParams, 'flowAmount'>;
+  [EmailTemplate.RECURRING_TX_REMINDER]: Pick<
+    TransactionParams,
+    'flowAmount' | 'receiverAddress' | 'transactionDate'
+  >;
+  [EmailTemplate.RECURRING_TX_SECOND_REMINDER]: Pick<
+    TransactionParams,
+    'flowAmount' | 'receiverName' | 'transactionDate'
+  >;
+  [EmailTemplate.RECURRING_TX_EXPIRATION_REMINDER]: Pick<
+    TransactionParams,
+    'flowAmount' | 'receiverName' | 'transactionDate'
+  >;
+  [EmailTemplate.RECURRING_TX_CANCELED]: Pick<
+    TransactionParams,
+    'receiverName'
+  >;
 };
 
 @Injectable()
@@ -39,16 +74,11 @@ export class EmailService {
     if (!templateId) {
       throw new Error('Email template not supported');
     }
-    const subject = args.subject
-      ? args.subject
-      : emailTemplateToDefaultSubject[args.template];
 
     this.logger.debug(
       `Sending ${args.template} email (to: ${
         args.to
-      }, subject: ${subject}, templateData: ${JSON.stringify(
-        args.templateData,
-      )})`,
+      }, templateData: ${JSON.stringify(args.templateData)})`,
     );
 
     if (process.env.ENABLE_SENDING_EMAIL !== 'true') {
@@ -56,7 +86,7 @@ export class EmailService {
     }
     return sendGrid.send({
       to: args.to,
-      subject,
+      // subject, TODO: subject field probably not necessary, because subject is already set in the design templates
       replyTo: {
         email: 'support@flowtea.me',
         name: 'FlowTea Support',
